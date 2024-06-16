55f14e1e47f52e5ad5cf1c9f394a03f4eed4fd16
CodeKiro <kirofiniello@outlook.it>
2024-06-16 19:49:04 +0200

    ho installato axios, e l'ho aggiunto ad AppMain, con il suo methodo, ora proseguo con l'analisi dei dati dalla chiamata get

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,39 @@
 <script>
 import MainSearch from './MainSearch.vue';
 import MainListCards from './MainListCards.vue';
+import axios from 'axios';
+
 export default {
-    components:{
+    components: {
         MainSearch,
         MainListCards
     },
     data() {
-        return 
-            {
+        return {
+            ListCards: [],
+        };
+    },
+        methods:{
+            getListCards(){
+                axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+            }
+        },
+        created(){
+            this.getListCards();
         }
     }
-}
+
 </script>
 
 <template>
