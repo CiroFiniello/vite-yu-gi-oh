172ffa213d1c280fcaa825934a07f2f7b73ca80e
CodeKiro <kirofiniello@outlook.it>
2024-06-16 18:48:27 +0200

    bootstrap aggiunto con gli use nell'app.vue

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,7 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss' as *;
-
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
